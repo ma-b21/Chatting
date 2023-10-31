e420cdc73aa5bc20ef29f67cde329260ad28226f
ma-b21 <mb_thu@163.com>
2023-10-31 11:09:55 +0800

    first commit, finish register and login

---
+++ Server/Bak/utils(9).asm
@@ -0,0 +1,60 @@
+include 		utils.inc
+
+.code
+CreateSocketForServer		PROC, port:DWORD
+	
+	LOCAL @addr:sockaddr_in
+	LOCAL @listenfd:DWORD
+	invoke socket,AF_INET, SOCK_STREAM, 0
+	.IF eax == -1
+		invoke MessageBox,NULL,OFFSET SOCKET_ERR,OFFSET ERR_TITLE,MB_OK
+		ret
+	.ENDIF
+	mov @listenfd, eax
+	
+	invoke crt_memset, ADDR @addr, 0, SIZEOF @addr
+	mov @addr.sin_family, AF_INET
+	mov @addr.sin_addr, INADDR_ANY
+	
+	invoke htons,port
+	mov @addr.sin_port,ax
+	
+	invoke bind,@listenfd,ADDR @addr,SIZEOF @addr
+	.IF eax == -1
+		invoke MessageBox,NULL,OFFSET SOCKET_ERR,OFFSET ERR_TITLE,MB_OK
+		ret
+	.ENDIF
+	mov eax, @listenfd
+	
+CreateSocketForServer	ENDP
+
+ConnectSocketForClient   	PROC, port:DWORD, ip:PTR BYTE
+	
+	LOCAL @addr:sockaddr_in
+	LOCAL @connfd:DWORD
+	
+	invoke socket,AF_INET, SOCK_STREAM,IPPROTO_TCP
+	.IF eax == -1
+		invoke MessageBox,NULL,OFFSET SOCKET_ERR,OFFSET ERR_TITLE,MB_OK
+		ret
+	.ENDIF
+	mov @connfd, eax
+	
+	invoke crt_memset,ADDR @addr, 0, SIZEOF @addr
+	mov @addr.sin_family,AF_INET
+	invoke htons,port
+	mov @addr.sin_port,ax
+	invoke inet_addr,ip
+	mov @addr.sin_addr,eax
+	
+	invoke connect,@connfd,ADDR @addr,SIZEOF @addr
+	.IF eax < 0
+		invoke MessageBox,NULL,OFFSET SOCKET_ERR,OFFSET ERR_TITLE,MB_OK
+		ret
+	.ENDIF
+	
+	mov eax,@connfd
+	ret
+
+ConnectSocketForClient ENDP
+END
